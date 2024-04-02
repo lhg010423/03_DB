8545dd29b99ca47d9b6e4da68fb3e992f65c4840
lhg010423 <lhg010423@gmail.com>
2024-04-02 17:48:26 +0900

    240402

--- sql모음/00_사용자_계정_생성.sql
+++ sql모음/00_사용자_계정_생성.sql
@@ -8,13 +8,13 @@ ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- CTRL + ENTER : 선택한 SQL 수행
 
 -- 사용자 계정 생성
-CREATE USER gwajae_lhg IDENTIFIED BY gwajae_lhg;
+CREATE USER kh_lhg IDENTIFIED BY kh_lhg;
 
 -- 사용자 계정에 권한 부여
-GRANT RESOURCE, CONNECT TO gwajae_lhg;
+GRANT RESOURCE, CONNECT TO kh_lhg;
 
 -- 객체가 생성될 수 있는 공간 할당량 지정
-ALTER USER gwajae_lhg DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER kh_lhg DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 --                                         무제한의 저장공간을 부여한다
 
 
